63548a75b2c56bd28bc046f96c3562435bde872d
jn2308879 <jimena.navarro27@live.com>
2016-06-23 23:31:18 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Jimena Navarro-Garcia
+ * Created on June 20, 2016, 6:51 PM 
+ * Purpose:  First Program
+ */
+
+//System Libraries
+#include <iostream>//Input/Outout Library 
+using namespace std;//Namespace of the System Libraries 
+
+//User Libraries
+
+//Global Constants 
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process the Data
+
+    //Output the processed Data
+    cout<<"Hello World"<<endl;
+
+    //Exit Stage Right!    
+    return 0;
+}
+
